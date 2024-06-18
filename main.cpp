da90a94643bcfff44b76c50ad1a6baa6a020b94d
Veronika Rassokhina <veronika_rassokhina@mail.ru>
2024-06-18 23:40:49 +0300

    Add triangle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <GL/glut.h>
 #include <iostream>
 
+void renderScene()
+{
+    glClearColor(0.0, 0.749, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(-0.5, -0.5, 0.0);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+    }
+    glEnd();
+
+    glutSwapBuffers();
+}
+
 int main(int argc, char **argv)
 {
     std::cout << "OpenGL Practice\n";
@@ -12,12 +28,9 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_STENCIL);
     glutCreateWindow("OpenGL Practice");
 
-    while (true)
-    {
-        /* see black window */
-    }
-
 	/* register callbacks */
+    glutDisplayFunc(renderScene);
 
 	/* enter GLUT event processing cycle */
+    glutMainLoop();
 }
