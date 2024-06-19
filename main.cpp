27e8ff33eb940b434ca9d23e16c13b55f59bd2ae
Veronika Rassokhina <veronika_rassokhina@mail.ru>
2024-06-19 12:58:02 +0300

    Add callback function for reshape

--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
 #include <GL/glut.h>
 #include <iostream>
 
-void renderScene()
-{
-    glClearColor(0.0, 0.749, 1.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-    {
-        glVertex3f(-0.5, -0.5, 0.0);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-    }
-    glEnd();
-
-    glutSwapBuffers();
-}
+void renderScene();
+void changeSize(int width, int height);
 
 int main(int argc, char **argv)
 {
@@ -24,13 +11,55 @@ int main(int argc, char **argv)
     /* init GLUT and create window */
     glutInit(&argc, argv);
     glutInitWindowPosition(100, 100);
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(700, 500);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_STENCIL);
     glutCreateWindow("OpenGL Practice");
 
 	/* register callbacks */
+    glutReshapeFunc(changeSize);
     glutDisplayFunc(renderScene);
 
 	/* enter GLUT event processing cycle */
     glutMainLoop();
+}
+
+void renderScene()
+{
+    glClearColor(0.0, 0.749, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(-0.5, -0.5, -5.0);
+        glVertex3f(0.5, 0.0, -5.0);
+        glVertex3f(0.0, 0.5, -5.0);
+    }
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void changeSize(int width, int height)
+{
+    // Prevent a divide by zero
+    if (height == 0)
+    {
+        height = 1;
+    }
+    double ratio = 1.0 * width / height;
+
+    // Use the Projection Matrix
+    glMatrixMode(GL_PROJECTION);
+
+    // Reset Matrix
+    glLoadIdentity();
+
+    // Set the viewport to be the entire window
+    glViewport(0, 0, width, height);
+
+    // Set the correct perspective
+    gluPerspective(45, ratio, 1, 1000);
+
+    // Get Back to the Modelview
+    glMatrixMode(GL_MODELVIEW);
 }
